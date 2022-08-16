b73dbf0ca8190b780c05b777d72a57eb0c28118f
gcoulby <gcoulby@googlemail.com>
2022-08-16 20:51:12 +0100

    trying fix for gh-pages

--- src/components/imageBox.jsx
+++ src/components/imageBox.jsx
@@ -3,7 +3,7 @@ function ImageBox({ image, word }) {
     return (
         <>
             <div className="image-box">
-                <img id="image" src={`./img/${image}`} alt={`image of ${word}`} />
+                <img id="image" src={`./jennifers-picture-generator/img/${image}`} alt={`image of ${word}`} />
             </div>
         </>
     );
